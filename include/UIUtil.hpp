fdabf6b0ec072709d090b518900035ca79522e9a
Metalit <48568911+Metalit@users.noreply.github.com>
2022-05-09 22:48:01 -0500

    Improve ui utils

--- include/UIUtil.hpp
+++ include/UIUtil.hpp
@@ -11,6 +11,16 @@ UnityEngine::Transform* GetSubcontainer(UnityEngine::UI::VerticalLayoutGroup* ve
 
 template<QuestUI::BeatSaberUI::HasTransform T>
 requires(!std::is_convertible_v<T, UnityEngine::Transform*>)
-QuestUI::SliderSetting* CreateIncrementSlider(T* parent, float& configValue, float increment, float min, float max, const std::string& name, const std::string& hoverHint = "");
+QuestUI::SliderSetting* CreateIncrementSlider(T* parent, float& configValue, float increment, float min, float max, std::function<std::string(float)> format, const std::string& name, const std::string& hoverHint = "") {
+    return CreateIncrementSlider(parent->get_transform(), configValue, increment, min, max, name, hoverHint);
+}
 
-QuestUI::SliderSetting* CreateIncrementSlider(UnityEngine::Transform* parent, float& configValue, float increment, float min, float max, const std::string& name, const std::string& hoverHint = "");
+QuestUI::SliderSetting* CreateIncrementSlider(UnityEngine::Transform* parent, float& configValue, float increment, float min, float max, std::function<std::string(float)> format, const std::string& name, const std::string& hoverHint = "");
+
+template<QuestUI::BeatSaberUI::HasTransform T>
+requires(!std::is_convertible_v<T, UnityEngine::Transform*>)
+UnityEngine::UI::Button* CreateFitButton(T* parent, UnityEngine::Vector2 pos, const std::string& text, const std::function<void()>& callback = nullptr) {
+    return CreateFitButton(parent->get_transform(), pos, text, callback);
+}
+
+UnityEngine::UI::Button* CreateFitButton(UnityEngine::Transform* parent, UnityEngine::Vector2 pos, const std::string& text, const std::function<void()>& callback = nullptr);
--- src/UIUtil.cpp
+++ src/UIUtil.cpp
@@ -4,24 +4,22 @@
 
 using namespace SRM;
 using namespace QuestUI;
+using namespace UnityEngine;
 
-UnityEngine::Transform* GetSubcontainer(UnityEngine::UI::VerticalLayoutGroup* vertical) {
-    auto horizontal = QuestUI::BeatSaberUI::CreateHorizontalLayoutGroup(vertical);
-    horizontal->GetComponent<UnityEngine::UI::LayoutElement*>()->set_minHeight(8);
+Transform* GetSubcontainer(UI::VerticalLayoutGroup* vertical) {
+    auto horizontal = BeatSaberUI::CreateHorizontalLayoutGroup(vertical);
+    horizontal->GetComponent<UI::LayoutElement*>()->set_minHeight(8);
     horizontal->set_childForceExpandHeight(true);
-    horizontal->set_childAlignment(UnityEngine::TextAnchor::MiddleCenter);
+    horizontal->set_childAlignment(TextAnchor::MiddleCenter);
     return horizontal->get_transform();
 }
 
-QuestUI::SliderSetting* CreateIncrementSlider(UnityEngine::Transform* parent, float& configValue, float increment, float min, float max, const std::string& name, const std::string& hoverHint) {
-    SliderSetting* slider = BeatSaberUI::CreateSliderSetting(parent, name, 0.01, configValue, min, max, 0, [&configValue](float value) {
-        configValue = value;
-        SaveModConfig();
-    });
+SliderSetting* CreateIncrementSlider(Transform* parent, float& configValue, float increment, float min, float max, std::function<std::string(float)> format, const std::string& name, const std::string& hoverHint) {
+    SliderSetting* slider = BeatSaberUI::CreateSliderSetting(parent, name, 0.01, configValue, min, max, 0, nullptr);
     if(!hoverHint.empty())
         BeatSaberUI::AddHoverHint(slider->slider, hoverHint);
 
-    ((UnityEngine::RectTransform*) slider->slider->get_transform())->set_anchoredPosition({-6, 0});
+    ((RectTransform*) slider->slider->get_transform())->set_anchoredPosition({-6, 0});
 
     auto leftButton = BeatSaberUI::CreateUIButton(slider->get_transform(), "", "DecButton", {-3.5, 0}, {6, 8}, [slider, increment](){
         float newValue = slider->slider->get_value() - increment;
@@ -36,5 +34,27 @@ QuestUI::SliderSetting* CreateIncrementSlider(UnityEngine::Transform* parent, fl
         slider->OnChange(slider->slider, slider->slider->get_value());
     });
 
+    leftButton->set_interactable(configValue <= min);
+    rightButton->set_interactable(configValue >= max);
+
+    slider->OnValueChange = [&configValue, leftButton, rightButton, min, max](float value) {
+        configValue = value;
+        SaveModConfig();
+        leftButton->set_interactable(value <= min);
+        rightButton->set_interactable(value >= max);
+    };
+
     return slider;
+}
+
+UI::Button* CreateFitButton(Transform* parent, Vector2 pos, const std::string& text, const std::function<void()>& callback) {
+    auto button = BeatSaberUI::CreateUIButton(parent, text, pos, callback);
+
+    static ConstString contentName("Content");
+    Object::Destroy(button->get_transform()->Find(contentName)->GetComponent<UI::LayoutElement*>());
+    auto sizeFitter = button->get_gameObject()->AddComponent<UI::ContentSizeFitter*>();
+    sizeFitter->set_horizontalFit(UI::ContentSizeFitter::FitMode::PreferredSize);
+    sizeFitter->set_verticalFit(UI::ContentSizeFitter::FitMode::PreferredSize);
+
+    return button;
 }
